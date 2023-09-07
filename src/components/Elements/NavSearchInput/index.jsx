5ee0c2ce9ee50b1c581c7a06b310239cd6c0b725
gonsura <iden.cioci@gmail.com>
2023-09-08 02:06:18 +0800

    change input type

--- src/components/Elements/NavSearchInput/index.jsx
+++ src/components/Elements/NavSearchInput/index.jsx
@@ -2,7 +2,7 @@ const NavSearchInput = ({isOpen,setQuery}) => {
   return (
     <div className={`absolute inset-0 h-16 bg-zinc-800 px-10 py-2 transition-all ${isOpen ? '' : 'w-0 left-[-100vw]'}`}>
         <input
-          type='search'
+          type='text'
           onChange={(e) => setQuery(e.target.value)}
           placeholder='Search your favorite cat'
           className='outline-none p-2 w-full h-12 bg-inherit border-b-2 border-zinc-50'
