2af7f9808024641e7f2edfe25621eadbe1b9081e
gonsura <iden.cioci@gmail.com>
2023-09-07 16:32:51 +0800

    modify counter

--- src/components/Fragments/main/index.jsx
+++ src/components/Fragments/main/index.jsx
@@ -5,23 +5,40 @@ const Main = () => {
   const [cats, setCats] = useState([])
   useEffect(() => {
     fetch('https://api.thecatapi.com/v1/breeds')
-    .then((res) => res.json())
-    .then((data) => {
-      setCats(data)
-    })
+      .then((res) => res.json())
+      .then((data) => {
+        setCats(data)
+      })
   }, [])
+
+  const decrement = () => {
+    if (counter > 0) setCounter((prevCount) => prevCount - 1)
+  }
+const increment = () => {
+  if (counter < 10)
+  setCounter((prevCount) => prevCount + 1)
+}
 
   return (
     <main className='p-4 text-2xl text-zinc-800 bg-zinc-400 min-h-[calc(100vh-4rem)]'>
       <div>
-        <button onClick={() => setCounter(counter - 1)} className='bg-zinc-700 px-4'>-</button>
+        <button onClick={decrement} className='bg-zinc-700 px-4'>
+          -
+        </button>
         <span className='mx-6'>{counter}</span>
-        <button onClick={() => setCounter(counter + 1)} className='bg-zinc-700 px-4'>+</button>
+        <button
+          onClick={increment}
+          className='bg-zinc-700 px-4'
+        >
+          +
+        </button>
       </div>
       <h1 className='text-4xl font-semibold'>list Cats</h1>
       <ul className='flex flex-wrap gap-x-4'>
         {cats.map((cat) => (
-          <li className='p-2 bg bg-zinc-50 mt-2 rounded-md' key={cat.id}>{cat.name}</li>
+          <li className='p-2 bg bg-zinc-50 mt-2 rounded-md' key={cat.id}>
+            {cat.name}
+          </li>
         ))}
       </ul>
     </main>
